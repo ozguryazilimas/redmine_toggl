804e2ada7f7a22285241bcc02ad914a04e3f0df7
Onur Küçük <onur@delipenguen.net>
2017-01-10 18:03:52 +0300

    cleanup issue number from time_entry comments

--- app/models/toggl_entry.rb
+++ app/models/toggl_entry.rb
@@ -79,7 +79,7 @@ class TogglEntry < ActiveRecord::Base
         :user => self.user,
         :hours => duration.to_f / 3600,
         :spent_on => start,
-        :comments => description
+        :comments => description.gsub(ISSUE_MATCHER, ' ')
       }
 
       self.time_entry ||= build_time_entry
