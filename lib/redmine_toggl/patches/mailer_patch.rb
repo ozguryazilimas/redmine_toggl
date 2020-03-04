5ee899e2ce03cf4d02f7d1da7806e34f99136158
Onur Küçük <onur@delipenguen.net>
2020-03-04 22:46:23 +0300

    add start stop times to report email subjects

--- lib/redmine_toggl/patches/mailer_patch.rb
+++ lib/redmine_toggl/patches/mailer_patch.rb
@@ -14,24 +14,35 @@ module RedmineToggl
 
       module InstanceMethods
 
-        def toggl_report_without_issue(_user, recipients, report_results, language = nil)
+        def toggl_report_without_issue(_user, recipients, report_results, language = nil, started_after = nil, stopped_before = nil)
           Rails.logger.info "Sending Toggl entry without issue report to #{recipients.inspect}"
           @report_data = report_results
           I18n.locale = language if language
           redmine_headers 'Report' => 'toggl_entries_without_issue'
 
-          mail :to => recipients, :subject => t('toggl.toggl_report_without_issue_subject')
+          subject = toggl_add_time_to_subject(t('toggl.toggl_report_without_issue_subject'), started_after, stopped_before)
+          mail :to => recipients, :subject => subject
         end
 
-        def toggl_report_without_project(_user, recipients, report_results, language = nil)
+        def toggl_report_without_project(_user, recipients, report_results, language = nil, started_after = nil, stopped_before = nil)
           Rails.logger.info "Sending Toggl entry without project report to #{recipients.inspect}"
           @report_data = report_results
           I18n.locale = language if language
           redmine_headers 'Report' => 'toggl_entries_without_project'
 
-          mail :to => recipients, :subject => t('toggl.toggl_report_without_project_subject')
+          subject = toggl_add_time_to_subject(t('toggl.toggl_report_without_project_subject'), started_after, stopped_before)
+          mail :to => recipients, :subject => subject
         end
 
+
+        def toggl_add_time_to_subject(subject_base, started_after = nil, stopped_before = nil)
+          return subject_base if started_after.blank? && stopped_before.blank?
+
+          start_str = started_after.strftime('%Y-%m-%d %H:%M') if started_after.present?
+          stop_str = (stopped_before || Time.now).strftime('%Y-%m-%d %H:%M')
+
+          "#{subject_base} (#{start_str} - #{stop_str})"
+        end
       end
     end
   end
